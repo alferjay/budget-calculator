261b37964f2a9ba59b54a0c2744c55bb7ac40664
Alfer Jay Salvador <alferjaysalvador@gmail.com>
2020-05-04 14:04:39 +0400

    Updates

---
+++ precache-manifest.19d25262ba355ad2e4a174005aed434a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e20ac5c7a993cb02e0a98a0393b2247",
+    "url": "/budget-calculator/index.html"
+  },
+  {
+    "revision": "3bd989722fca6231c00f",
+    "url": "/budget-calculator/static/css/main.2387b08b.chunk.css"
+  },
+  {
+    "revision": "bbf26ad58213f302905c",
+    "url": "/budget-calculator/static/js/2.e342f51a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget-calculator/static/js/2.e342f51a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd989722fca6231c00f",
+    "url": "/budget-calculator/static/js/main.7afb7bec.chunk.js"
+  },
+  {
+    "revision": "4d768fb26ce3376359da",
+    "url": "/budget-calculator/static/js/runtime-main.6943ab90.js"
+  }
+]);
